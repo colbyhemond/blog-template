f3df60e25fb5df9de07956a4ee0cd8d71aed2b9f
Colby Hemond <hemond.colby@gmail.com>
2023-03-10 07:25:28 -0500

    add decap cms with netlify identity

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+    <Component {...pageProps} />
+  </>)
 }
